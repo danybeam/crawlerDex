321d103f338b3d13385e1d3e33ec2b089c5e55ee
Daniel Orozco <danybeam@gmail.com>
2019-07-11 18:58:57 -0500

    changed request to method, changed loop exit condition, changed save method to build link dinamically

--- crawlerDex/webCrawler/crawler.py
+++ crawlerDex/webCrawler/crawler.py
@@ -7,9 +7,27 @@ import urllib.request as urllib
 def getPKMNS():
     baseUrl = 'https://bulbapedia.bulbagarden.net'
     startUrl = '/wiki/Bulbasaur_(Pok%C3%A9mon)'
-    savePKMN(startUrl)
     url = baseUrl + startUrl
-    stop = False
+
+    soup,link = requestPage(url)
+    print(link)
+    input()
+
+    while link != startUrl and link != '/wiki/%3F%3F%3F_(Pok%C3%A9mon)' and link != '/wiki/Pok%C3%A9mon_(species)':
+        url = baseUrl + link
+        soup,link = requestPage(url)
+        print(link)
+        input()
+
+    print('end')
+
+def savePKMN(data,pokemon="Missingno",filePath=os.path.abspath(__file__)[:-10] + "..\\data\\"):
+    filePath = filePath + pokemon + ".dex"
+    dex = open(filePath,'a+')
+    dex.write(data)
+    dex.close()
+
+def requestPage(url):
     req = urllib.Request(
         url, 
         data=None, 
@@ -21,32 +39,9 @@ def getPKMNS():
     soup = BeautifulSoup(html,features="html.parser")
     link = soup.body.find_next('a',title=re.compile(r'. \(Pokémon\)'),string='→')
 
-    while link['href'] != startUrl or not stop:
-        savePKMN(link['href'])
-        url = baseUrl + link['href']
-        stop = True
-        req = urllib.Request(
-            url, 
-            data=None, 
-            headers={
-                'User-Agent': 'Mozilla/5.0'
-            }
-        )
-        html = urllib.urlopen(req)
-        soup = BeautifulSoup(html,features="html.parser")
-        link = soup.body.find_next('a',title=re.compile(r'. \(Pokémon\)'),string='→')
-    print('end')
-
-def savePKMN(data,filePath=''):
-    filePath = os.getcwd
-    dex = open(filePath,'a+')
-    dex.write(data)
-    dex.close()
+    return soup, link["href"]
 
     
 if __name__ == "__main__":
     print()
-    print(os.path.dirname(os.path.relpath(__file__)))
-    print(os.path.dirname(os.path.abspath(__file__)))
-    input()
     getPKMNS()
